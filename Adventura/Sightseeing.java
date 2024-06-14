a0140094c9ebcb2190d2daf5c13d34c06c057ec2
wahajrashed <wahajrashed1@gmail.com>
2024-06-14 19:40:21 +0500

    Update Sightseeing.java

--- Adventura/Sightseeing.java
+++ Adventura/Sightseeing.java
@@ -1,11 +1,11 @@
 // Concrete class representing a Sightseeing location
 class Sightseeing extends Place {
-
-    // Constructor
+    // Constructor to initialize the Sightseeing with name and location
     public Sightseeing(String name, String location) {
         super(name, location, "Sightseeing");
     }
 
+    // Overridden method to display details of the Sightseeing
     @Override
     public void displayDetails() {
         System.out.println("Sightseeing: " + name);
