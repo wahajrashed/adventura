f660ea56d5bf665f49aec085aed07165ed9adb52
wahajrashed <wahajrashed1@gmail.com>
2024-06-14 19:40:40 +0500

    Update MatchService.java

--- Adventura/MatchService.java
+++ Adventura/MatchService.java
@@ -1,4 +1,5 @@
 // Interface for matching users based on preferences
 interface MatchService {
+    // Method to connect two users based on their preferences
     void connectUsers(User user1, User user2);
 }
